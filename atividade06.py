c1e99c7c28c7f9460f80d2a7dba1e1ca2a0c4bd1
LEONARDO KAWASAKI <leonardokws@edu.unifil.br>
2019-06-08 23:57:50 -0300

    Update atividade06.py

--- atividade06.py
+++ atividade06.py
@@ -194,6 +194,103 @@ def imagem_gaussian3(i, j, imagem):
     
     return imagem
 
+def imagem_gaussian5(i, j, imagem):
+    leste = (imagem[i+1][j] * 0.26)
+    oeste = (imagem[i-1][j] * 0.26)
+    norte = (imagem[i][j-1] * 0.26)
+    sul = (imagem[i][j+1] * 0.26)
+    centro = (imagem[i][j] * 0.4)
+    nordeste = (imagem[i+1][j-1] * 0.12)
+    noroeste = (imagem[i-1][j-1] * 0.12)
+    sudeste = (imagem[i+1][j+1] * 0.12)
+    sudoeste = (imagem[i-1][j+1] * 0.12)
+    leste5 = (imagem[i+2][j] * 0.06)
+    oeste5 = (imagem[i-2][j] * 0.06)
+    norte5 = (imagem[i][j-2] * 0.06)
+    sul5 = (imagem[i][j+2] * 0.06)
+    nordeste5 = (imagem[i+2][j-2] * 0.01)
+    noroeste5 = (imagem[i-2][j-2] * 0.01)
+    sudeste5 = (imagem[i+2][j+2] * 0.01)
+    sudoeste5 = (imagem[i-2][j+2] * 0.01)
+    nordeste5lado = (imagem[i+2][j-1] * 0.03)
+    noroeste5lado = (imagem[i-2][j-1] * 0.03)
+    norte5esq = (imagem[i-1][j-2] * 0.03)
+    norte5dir = (imagem[i+1][j-2] * 0.03)
+    sudeste5lado = (imagem[i+2][j+1] * 0.03)
+    sudoeste5lado = (imagem[i-2][j-1] * 0.03)
+    sul5esq = (imagem[i-1][j+2] * 0.03)
+    sul5dir = (imagem[i+1][j+2] * 0.03)
+    media = [leste,oeste,norte,sul,centro,nordeste,noroeste,sudeste,sudoeste,
+             leste5,oeste5,norte5,sul5,nordeste5,noroeste5,sudeste5,sudoeste5,
+             nordeste5lado,noroeste5lado,norte5esq,norte5dir,sudeste5lado,sudoeste5lado,
+             sul5esq,sul5dir]
+    # imagem = (sum(media))
+    imagem = (sum(media) // len(media))
+    
+    return imagem
+
+def imagem_gaussian7(i, j, imagem):
+    leste = (imagem[i+1][j] * 0.52)
+    oeste = (imagem[i-1][j] * 0.52)
+    norte = (imagem[i][j-1] * 0.52)
+    sul = (imagem[i][j+1] * 0.52)
+    centro = (imagem[i][j] * 0.8)
+    nordeste = (imagem[i+1][j-1] * 0.24)
+    noroeste = (imagem[i-1][j-1] * 0.24)
+    sudeste = (imagem[i+1][j+1] * 0.24)
+    sudoeste = (imagem[i-1][j+1] * 0.24)
+    leste5 = (imagem[i+2][j] * 0.12)
+    oeste5 = (imagem[i-2][j] * 0.12)
+    norte5 = (imagem[i][j-2] * 0.12)
+    sul5 = (imagem[i][j+2] * 0.12)
+    nordeste5 = (imagem[i+2][j-2] * 0.06)
+    noroeste5 = (imagem[i-2][j-2] * 0.06)
+    sudeste5 = (imagem[i+2][j+2] * 0.06)
+    sudoeste5 = (imagem[i-2][j+2] * 0.06)
+    nordeste5lado = (imagem[i+2][j-1] * 0.06)
+    noroeste5lado = (imagem[i-2][j-1] * 0.06)
+    norte5esq = (imagem[i-1][j-2] * 0.06)
+    norte5dir = (imagem[i+1][j-2] * 0.06)
+    sudeste5lado = (imagem[i+2][j+1] * 0.06)
+    sudoeste5lado = (imagem[i-2][j-1] * 0.06)
+    sul5esq = (imagem[i-1][j+2] * 0.06)
+    sul5dir = (imagem[i+1][j+2] * 0.06)
+    leste7 = (imagem[i+3][j] * 0.06)
+    oeste7 = (imagem[i-3][j] * 0.06)
+    norte7 = (imagem[i][j-3] * 0.06)
+    sul7 = (imagem[i][j+3] * 0.06)
+    nordeste7 = (imagem[i+3][j-3] * 0.01)
+    noroeste7 = (imagem[i-3][j-3] * 0.01)
+    sudeste7 = (imagem[i+3][j+3] * 0.01)
+    sudoeste7 = (imagem[i-3][j+3] * 0.01)
+    leste7cima = (imagem[i+3][j-1] * 0.03)
+    leste7baixo = (imagem[i+3][j+1] * 0.03)
+    oeste7cima = (imagem[i-3][j-1] * 0.03)
+    oeste7baixo = (imagem[i-3][j+1] * 0.03)
+    nordeste7lado = (imagem[i+3][j-2] * 0.03)
+    sudeste7lado = (imagem[i+3][j+2] * 0.03)
+    noroeste7lado = (imagem[i-3][j-2] * 0.03)
+    sudoeste7lado = (imagem[i-3][j+2] * 0.03)
+    norte7dir1 = (imagem[i+1][j-3] * 0.03)
+    norte7dir2 = (imagem[i+2][j-3] * 0.03)
+    norte7esq1 = (imagem[i-1][j-3] * 0.03)
+    norte7esq2 = (imagem[i-2][j-3] * 0.03)
+    sul7dir1 = (imagem[i+1][j+3] * 0.03)
+    sul7dir2 = (imagem[i+2][j+3] * 0.03)
+    sul7esq1 = (imagem[i-1][j+3] * 0.03)
+    sul7esq2 = (imagem[i-2][j+3] * 0.03)
+    media = [leste,oeste,norte,sul,centro,nordeste,noroeste,sudeste,sudoeste,
+             leste5,oeste5,norte5,sul5,nordeste5,noroeste5,sudeste5,sudoeste5,
+             nordeste5lado,noroeste5lado,norte5esq,norte5dir,sudeste5lado,sudoeste5lado,
+             sul5esq,sul5dir,leste7,oeste7,norte7,sul7,nordeste7,noroeste7,sudeste7,
+             sudoeste7,leste7cima,leste7baixo,oeste7cima,oeste7baixo,nordeste7lado,
+             sudeste7lado,noroeste7lado,sudoeste7lado,norte7dir1,norte7dir2,
+             norte7esq1,norte7esq2,sul7dir1,sul7dir2,sul7esq1,sul7esq2]
+    # imagem = (sum(media))
+    imagem = (sum(media) // len(media))
+    
+    return imagem
+
 def image_filter(imgfilter, image):
     ''' Os seguintes filtros deverão ser implementados:
         - mediana
@@ -203,8 +300,6 @@ def image_filter(imgfilter, image):
         - mean3
         - mean5
         - mean7
-        - prewitt
-        - sobel
     '''
     img_array = array(img)
     img_arrayalt = array(img)
@@ -244,13 +339,31 @@ def image_filter(imgfilter, image):
         
         imagem_gaussian3(i, j, image)
         
+    if imgfilter == 'gaussian5':
+        for i in range(len(img_array) -2):
+            for j in range(len(img_array[i]) -2):
+                img_arrayalt[i][j] = imagem_gaussian5(i,j,img_array)
+        new_image = Image.fromarray(img_arrayalt)
+        
+        imagem_gaussian5(i, j, image)
+        
+    if imgfilter == 'gaussian7':
+        for i in range(len(img_array) -3):
+            for j in range(len(img_array[i]) -3):
+                img_arrayalt[i][j] = imagem_gaussian7(i,j,img_array)
+        new_image = Image.fromarray(img_arrayalt)
+        
+        imagem_gaussian7(i, j, image)
+    
     return new_image
     
 mean3 = image_filter('mean3', img_array)
 mean5 = image_filter('mean5', img_array)
 mean7 = image_filter('mean7', img_array)
 mediana = image_filter('mediana', img_array)
-gaussian = image_filter('gaussian3', img_array)
+gaussian3 = image_filter('gaussian3', img_array)
+gaussian5 = image_filter('gaussian5', img_array)
+gaussian7 = image_filter('gaussian7', img_array)
 
 image_noise('white', img_array1)
 noise_white = Image.fromarray(img_array1)
@@ -282,12 +395,14 @@ plt.subplot(437)
 plt.imshow(mediana)
 plt.title('Mediana')
 plt.subplot(438)
-plt.imshow(gaussian)
+plt.imshow(gaussian3)
 plt.title('Gaussian 3')
 plt.subplot(439)
-plt.imshow(img)
+plt.imshow(gaussian5)
+plt.title('Gaussian 5')
 plt.subplot(4,3,10)
-plt.imshow(img)
+plt.imshow(gaussian7)
+plt.title('Gaussian 7')
 plt.subplot(4,3,11)
 plt.imshow(img)
 plt.subplot(4,3,12)
